8daecf9e48e156444e0e0ba925d6bd6187123c31
gulnaz <gulnazhakimova70@gmail.com>
2025-11-11 16:40:43 +0300

    try buttons#3

--- frontend/src/components/product-list/index.jsx
+++ frontend/src/components/product-list/index.jsx
@@ -66,14 +66,13 @@ const ProductList = () => {
     setAddedItems(newItems);
 
     if (newItems.length === 0) {
-      //tg.MainButton.hide();
-      tg.BackButoon.hide();
-      //tg.SettingsButton.hide();
+      // tg.MainButton.hide();
+      // tg.BackButoon.hide();
+      tg.SettingsButton.hide();
     } else {
       //tg.MainButton.show();
-      tg.BackButoon.show();
-      //tg.SettingsButton.show();
-      //tg.MainButton.setParams({
+      tg.SettingsButton.show();
+      // tg.MainButton.setParams({
       //   text: `Купить ${getTotalPrice(newItems)}`
       // });
     }

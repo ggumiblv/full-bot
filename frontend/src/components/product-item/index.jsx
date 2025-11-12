1f43828134adda32d422cadfd33fa57e759df573
gulnaz <gulnazhakimova70@gmail.com>
2025-11-12 11:19:32 +0300

    fixed product list styles#2

--- frontend/src/components/product-item/index.jsx
+++ frontend/src/components/product-item/index.jsx
@@ -9,15 +9,15 @@ const ProductItem = ({ product, className, onAdd }) => {
 
   return (
     <div className={'product ' + className}>
-      <div className={'img'} />
-      <div className={'title'}>{product.title}</div>
-      <div className={'description'}>{product.description}</div>
-      <div className={'price'}>
+      <div className="image" />
+      <div className="title">{product.title}</div>
+      <div className="description">{product.description}</div>
+      <div className="price">
         <span>
           Стоимость: <b>{product.price}</b>
         </span>
       </div>
-      <Button className={'add-btn'} onClick={onAddHandler}>
+      <Button className="add-button" onClick={onAddHandler}>
         Добавить в корзину
       </Button>
     </div>
